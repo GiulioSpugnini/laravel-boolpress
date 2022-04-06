f0c5d3c361ed37396edc849a06834df32714bc49
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-04-06 18:32:26 +0200

    created Alert component

---
+++ resources/js/components/posts/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="`alert alert-${type}||'info`" role="alert">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: ["type", "message"],
+};
+</script>
+
+<style>
+</style>
